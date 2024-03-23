673b4974a1fc11527a338eaef3f5c94a74fce579
Manikanta Satya Subbareddy Kovvuri <50864579+subbareddykovvuri@users.noreply.github.com>
2024-03-23 23:09:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express =require('express')
+const bodyParser=require('body-parser')
+const request=require('request')
+
+const app=express()
+
+const apikey='';
+app.unsubscribe(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+app.get('/',function(req,res){
+    res.render('index',{weather:null,error:null})
+}) 
+
+app.post('/',function(req,res){
+    let city=req.body.city
+    let url='https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={apikey}'
+    console.log(req.body.city);
+    request(url,function(err,response,body){
+        if(err){
+            res.render('index',{weather:null,error:'Error,Please try again'})
+
+        }
+        else{
+            let weather=JSON.parse(body)
+            if(weather.main==undefined){
+                res.render('index',{
+                    weather:null,
+                    error:'Error, please try again',
+                });
+            }else{
+                let weatherText='It ${weather.main.temp} degrees celsius with ${weather.weather[0].main} in ${weather.name}';
+                res.render('index',{weather:weatherText,error:null});
+                console.log('body:',body);
+            }
+        }
+    })
+})
+
+app.listen(3000,function(){
+    console.log("weatherly app listening on port 3000");
+});
