af66a3128042417612397f9e21b6ba36d76da2a8
Manikanta Satya Subbareddy Kovvuri <50864579+subbareddykovvuri@users.noreply.github.com>
2024-03-24 23:55:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const WebSocket = require('ws');
+var router=express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -25,8 +27,21 @@ const colorSchema = new mongoose.Schema({
 const Color = mongoose.model('Color', colorSchema);
 
 // Middleware to parse JSON bodies
-//app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.json());
 app.use(express.static('views'));
+
+// WebSocket server
+const wss = new WebSocket.Server({ port:8080});
+
+// Broadcast color data updates to all connected clients
+function broadcastColorDataUpdate(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(data));
+        }
+    });
+}
 
 
 // Route to handle incoming color data
@@ -47,6 +62,13 @@ app.post('/api/colors', async (req, res) => {
 
         // Save or update the color data
         await existingColor.save();
+        // Fetch all colors from the database
+        const allColors = await Color.find();
+        const colors = allColors.map(item => item.color);
+        const counts = allColors.map(item => item.count);
+
+        // Broadcast color data updates to all connected clients
+        broadcastColorDataUpdate({ colors, counts });
 
         res.status(200).send('Color data saved successfully');
     } catch (error) {
@@ -64,6 +86,30 @@ app.get('/api/colors', async (req, res) => {
         console.error('Error fetching color data:', error);
         res.status(500).send('An error occurred while fetching color data');
     }
+});
+
+// WebSocket server event listeners
+wss.on('connection', function connection(ws) {
+    console.log('Client connected');
+    
+    // Send initial color data when a client connects
+    Color.find({}, function(err, colors) {
+        if (err) {
+            console.error('Error fetching color data:', err);
+            return;
+        }
+
+        const colorData = {
+            colors: colors.map(item => item.color),
+            counts: colors.map(item => item.count),
+        };
+        
+        ws.send(JSON.stringify(colorData));
+    });
+
+    ws.on('close', function close() {
+        console.log('Client disconnected');
+    });
 });
 
 // Start the server
