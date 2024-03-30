2707a648da860b060ec603dd01efd52e6d0de2f8
Manikanta Satya Subbareddy Kovvuri <50864579+subbareddykovvuri@users.noreply.github.com>
2024-03-30 18:10:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// server.js
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -83,6 +82,22 @@ app.post('/api/colors', async (req, res) => {
     }
 });
 
+app.delete('/api/colors',async(req,res)=>{
+    try {
+        console.log("Deleting data");
+        // Update count of all colors to zero
+        await Color.updateMany({}, { $set: { count: 0 } });
+        console.log("Deleted data successfully");
+        // Broadcast color data updates to all connected clients
+        broadcastColorDataUpdate({ colors: [], counts: [] });
+
+        res.status(200).send('Color data cleared successfully');
+    } catch (error) {
+        console.error('Error clearing color data:', error);
+        res.status(500).send('An error occurred while clearing color data');
+    }
+})
+
 // Route to fetch color data
 app.get('/api/colors', async (req, res) => {
     try {
@@ -118,130 +133,7 @@ wss.on('connection', function connection(ws) {
     });
 });
 
-// Start the server
-//app.listen(PORT, () => {
-//    console.log(`Server is running on port ${PORT}`);
-//});
+//Start the Server
 server.listen(8080, function () {
     console.log('Listening on http://0.0.0.0:8080');
   });
-
-
-
-/*const express =require('express')
-const bodyParser=require('body-parser')
-const request=require('request')
-
-const app=express()
-
-const apikey='';
-app.unsubscribe(express.static('public'))
-app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine","ejs");
-
-app.get('/',function(req,res){
-    res.render('index',{weather:null,error:null})
-}) 
-
-app.post('/',function(req,res){
-    let city=req.body.city
-    let url='https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={apikey}'
-    console.log(req.body.city);
-    request(url,function(err,response,body){
-        if(err){
-            res.render('index',{weather:null,error:'Error,Please try again'})
-
-        }
-        else{
-            let weather=JSON.parse(body)
-            if(weather.main==undefined){
-                res.render('index',{
-                    weather:null,
-                    error:'Error, please try again',
-                });
-            }else{
-                let weatherText='It ${weather.main.temp} degrees celsius with ${weather.weather[0].main} in ${weather.name}';
-                res.render('index',{weather:weatherText,error:null});
-                console.log('body:',body);
-            }
-        }
-    })
-})
-
-app.listen(3000,function(){
-    console.log("weatherly app listening on port 3000");
-});*/
-/*
-// Import necessary modules
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-// Initialize Express app
-const app = express();
-
-// Middleware for parsing JSON bodies
-app.use(bodyParser.json());
-
-// MongoDB connection URI
-//const mongoURI = 'mongodb://localhost:27017/mydatabase'; // Change this to your MongoDB URI
-const mongoURI='mongodb+srv://subbareddy934:SFZp2ZON3TU9EDXb@weatherapp.7wvka59.mongodb.net/?retryWrites=true&w=majority&appName=WeatherApp';
-// Connect to MongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-
-// Check MongoDB connection
-db.once('open', () => {
-    console.log('Connected to MongoDB');
-});
-
-// Define a mongoose schema for your data
-const dataSchema = new mongoose.Schema({
-    temperature: Number,
-    humidity: Number,
-    // Add more fields as needed
-});
-
-// Create a mongoose model based on the schema
-const Data = mongoose.model('Data', dataSchema);
-
-app.get('/', async (req, res) => {
-    try {
-        // Retrieve data from MongoDB
-        const allData = await Data.find();
-        res.json(allData);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'An error occurred while fetching data' });
-    }
-});
-// Route to handle POST requests from ESP8266
-app.post('/data', (req, res) => {
-    // Extract data from the request body
-    const { temperature, humidity } = req.body;
-
-    // Create a new document using the Data model
-    const newData = new Data({
-        temperature,
-        humidity,
-        // Add more fields as needed
-    });
-
-    // Save the document to the database
-    newData.save((err) => {
-        if (err) {
-            console.error('Error saving data:', err);
-            res.status(500).send('Error saving data to database');
-        } else {
-            console.log('Data saved successfully');
-            res.status(200).send('Data saved successfully');
-        }
-    });
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
-*/
